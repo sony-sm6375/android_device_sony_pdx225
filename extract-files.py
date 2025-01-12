149e15fd7aaeff29bc71b607030c85bf6c6ce0c5
LuK1337 <priv.luk@gmail.com>
2025-01-12 15:39:02 +0100

    pdx225: Initial vendor blobs
    Change-Id: I835cf4166113a185ebab745f3f6ababec1255720

--- extract-files.py
+++ extract-files.py
@@ -4,19 +4,44 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
 
 namespace_imports = [
+    'hardware/sony',
+    'vendor/qcom/opensource/display',
     'vendor/sony/sm6375-common',
 ]
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/lib64/libmorpho_movie_stabilizer.so': blob_fixup()
+        .add_needed('libutils.so')
+        .clear_symbol_version('AHardwareBuffer_acquire')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_lockPlanes')
+        .clear_symbol_version('AHardwareBuffer_unlock'),
+}  # fmt: skip
 
 module = ExtractUtilsModule(
     'pdx225',
     'sony',
     namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
 )
 
 if __name__ == '__main__':
