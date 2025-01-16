560ab50f0072735b68e1bd33af4ef6b45cbad559
LuK1337 <priv.luk@gmail.com>
2025-01-16 21:28:31 +0100

    pdx225: Import device specific overlays
    Generated-with: https://github.com/ArianK16a/rro-utils
    Change-Id: Ia786b23a19ab162716abea1fc8eab06a546d7976

--- device.mk
+++ device.mk
@@ -17,6 +17,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_holi/sound_trigger_platform_info.xml
 
+# Overlay
+PRODUCT_PACKAGES += \
+    FrameworkRes-PDX225-Overlay \
+    SettingsProvider-PDX225-Overlay \
+    SystemUIRes-PDX225-Overlay
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
