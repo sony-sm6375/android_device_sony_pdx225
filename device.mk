3c70eb3f940d144afd93066158ac55e0728ed895
LuK1337 <priv.luk@gmail.com>
2025-01-17 14:40:16 +0100

    pdx225: Build Lineage lights HAL
    Change-Id: I75a7814adbf7e159993cab7bca4be09ef12e407e

--- device.mk
+++ device.mk
@@ -17,6 +17,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_holi/sound_trigger_platform_info.xml
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.lineage
+
 # Overlay
 PRODUCT_PACKAGES += \
     FrameworkRes-PDX225-Overlay \
